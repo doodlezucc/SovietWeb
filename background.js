819e4c904532961e02b435708275c0011e64b966
doodlezucc <electerminator@gmail.com>
2024-09-06 15:05:44 +0200

    Revert "update manifest to v3"
    This reverts commit 22e0cd6c670af091e12e3eee47ac090c297f8512.

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(function(request, sender) {
     if (request.action === "enable") {
-        chrome.action.setIcon({ path: 'icons/icon.png', tabId: sender.tab.id });
+        chrome.browserAction.setIcon({ path: 'icons/icon.png', tabId: sender.tab.id });
     }
 });
